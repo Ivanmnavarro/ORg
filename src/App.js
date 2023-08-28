7ab9bb648478c93bc9cda938df6624f6b8a1a13a
Ivan Navarro <ivanmnavarro@gmail.com>
2023-08-28 10:19:12 -0300

    formulario-ingreso input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import MiOrg from './components/MiOrg';
+import Equipo from './components/Equipo';
+import Footer from './components/Footer';
 
 function App() {
+
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+  //resgistrar colaborador
+
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
+  }
+
+  //lista equipos
+
+  const equipos = [
+    {
+      titulo:"Programación",
+      colorPrimario:"#57C278",
+      colorSecundario:"#D9F7E9"
+    },
+    {
+      titulo:"Front End",
+      colorPrimario:"#82CFFA",
+      colorSecundario:"#E8F8FF"
+    },
+    {
+      titulo:"Data Science",
+      colorPrimario:"#A6D157",
+      colorSecundario:"#F0F8E2"
+    },
+    {
+      titulo:"Devops",
+      colorPrimario:"#E06B69",
+      colorSecundario:"#FDE7E8"
+    },
+    {
+      titulo:"Ux y Diseño",
+      colorPrimario:"#DB6EBF",
+      colorSecundario:"#FAE9F5"
+    },
+    {
+      titulo:"Movil",
+      colorPrimario:"#FFBA05",
+      colorSecundario:"#FFF5D9"
+    },
+    {
+      titulo:"Innovación y Gestión",
+      colorPrimario:"#FF8A29",
+      colorSecundario:"#FFEEDF"
+    }    
+        
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {
+      mostrarFormulario ? <Formulario 
+      equipos={equipos.map((equipo) => equipo.titulo)}/> : <></>}
+
+      
+      <MiOrg cambiarMostrar = {cambiarMostrar}/>
+      { 
+      equipos.map( (equipo) =><Equipo datos={equipo} key={equipo.titulo}/>)
+      }
+      
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,15 @@
+import "./Colaborador.css"
+
+const Colaborador = () => {
+    return <div className="colaborador">
+        <div className="encabezado">
+            <img src="https://github.com/Ivanmnavarro.png" alt="Ivan" />
+        </div>
+        <div className="info">
+            <h4>Iván Navarro</h4>
+            <h5>Instructor</h5>
+        </div>
+    </div>
+}
+
+export default Colaborador
