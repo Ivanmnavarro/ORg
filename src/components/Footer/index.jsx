a06b717ebb403a6c8eeed67e3148e2160d68b0e4
Ivan Navarro <ivanmnavarro@gmail.com>
2023-08-28 20:22:32 -0300

    cambio iconos redes footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,22 +1,25 @@
 import "./Footer.css"
+import { AiOutlineLinkedin } from "react-icons/ai"
+import { AiOutlineFacebook } from "react-icons/ai"
 
 const Footer = () => {
-    
-    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
-    <div className='redes'>
-        <a href='https://www.facebook.com/ivan.navarro.79069'>
-            <img src="/img/facebook.png" alt='Facebook' />
-        </a>
-        <a href='https://www.linkedin.com/in/ivanmnavarro/'>
-            <img src="/img/linkedin.png" alt='linkedin' />
-        </a>
-        
-    </div>
-    <img src='/img/Logo.png' alt='org' />
-    <strong>Desarrollado por Iván Navarro</strong>
-</footer>
 
-    
+    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
+        <div className='redes'>
+            <a href='https://www.facebook.com/ivan.navarro.79069'>
+                <AiOutlineFacebook size={40} color="white"/>
+
+            </a>
+            <a href='https://www.linkedin.com/in/ivanmnavarro/'>
+                <AiOutlineLinkedin size={40} color="white"/>
+            </a>
+
+        </div>
+        <img src='/img/Logo.png' alt='org' />
+        <strong>Desarrollado por Iván Navarro</strong>
+    </footer>
+
+
 }
 
 export default Footer
