4f9afe82808375100d6305274196a3b5414ac191
Ivan Navarro <ivanmnavarro@gmail.com>
2023-08-28 14:27:04 -0300

    Agregar colaboradores

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Formulario from './components/Formulario';
 import MiOrg from './components/MiOrg';
 import Equipo from './components/Equipo';
 import Footer from './components/Footer';
+import Colaborador from './components/Colaborador/index';
 
 function App() {
 
   const [mostrarFormulario, actualizarMostrar] = useState(false)
+  const [colaboradores, acualizarColaboradores] = useState([])
+
   const cambiarMostrar = () => {
     actualizarMostrar(!mostrarFormulario)
   }
@@ -16,63 +19,72 @@ function App() {
 
   const registrarColaborador = (colaborador) => {
     console.log("Nuevo colaborador", colaborador)
+
+    //Spread operator
+    acualizarColaboradores([...colaboradores, colaborador])
   }
 
   //lista equipos
 
   const equipos = [
     {
-      titulo:"Programación",
-      colorPrimario:"#57C278",
-      colorSecundario:"#D9F7E9"
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
     },
     {
-      titulo:"Front End",
-      colorPrimario:"#82CFFA",
-      colorSecundario:"#E8F8FF"
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
     },
     {
-      titulo:"Data Science",
-      colorPrimario:"#A6D157",
-      colorSecundario:"#F0F8E2"
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
     },
     {
-      titulo:"Devops",
-      colorPrimario:"#E06B69",
-      colorSecundario:"#FDE7E8"
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
     },
     {
-      titulo:"Ux y Diseño",
-      colorPrimario:"#DB6EBF",
-      colorSecundario:"#FAE9F5"
+      titulo: "Ux y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
     },
     {
-      titulo:"Movil",
-      colorPrimario:"#FFBA05",
-      colorSecundario:"#FFF5D9"
+      titulo: "Movil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
     },
     {
-      titulo:"Innovación y Gestión",
-      colorPrimario:"#FF8A29",
-      colorSecundario:"#FFEEDF"
-    }    
-        
-]
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+
+  ]
 
   return (
     <div className="App">
       <Header />
       {
-      mostrarFormulario ? <Formulario 
-      equipos={equipos.map((equipo) => equipo.titulo)}/> : <></>}
-
-      
-      <MiOrg cambiarMostrar = {cambiarMostrar}/>
-      { 
-      equipos.map( (equipo) =><Equipo datos={equipo} key={equipo.titulo}/>)
+        mostrarFormulario && <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+        />
       }
-      
-      <Footer/>
+
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+      {
+        equipos.map((equipo) => <Equipo
+          datos={equipo}
+          key={equipo.titulo}
+          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+        />)
+      }
+
+      <Footer />
     </div>
   );
 }
